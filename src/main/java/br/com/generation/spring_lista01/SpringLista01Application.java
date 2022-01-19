bf6d6e2b2e0b045f2f83a583341b9c8958c6f84e
josiasveras <josias_veras@hotmail.com>
2022-01-19 14:23:18 -0300

    Projeto criado

---
+++ src/main/java/br/com/generation/spring_lista01/SpringLista01Application.java
@@ -0,0 +1,13 @@
+package br.com.generation.spring_lista01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLista01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLista01Application.class, args);
+	}
+
+}
